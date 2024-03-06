3d21e5deb561a5e3466f25d58dc0042ff8649880
robinsch <hello@robinsch.com>
2024-03-06 18:02:25 +0100

    Update some naming

--- AmbientMod.lua
+++ AmbientMod.lua
@@ -4,7 +4,7 @@ local addonName = ...
 
 AmbientModPanelOptions = {
 	worldTimeSpeed = { text = "Time Speed", minValue = 0.0166666675359011, maxValue = 75.0, valueStep = 0.5, },
-	ambientShade = { text = "Ambient Shade", minValue = 0.05, maxValue = 1, valueStep = 0.005, },
+	ambientShade = { text = "Ambient Darkness", minValue = 0.05, maxValue = 1, valueStep = 0.005, },
 	lightLinearAttenuation = { text = "Light Radius", minValue = 0.005, maxValue = 1, valueStep = 0.0025, },
 	lightQuadraticAttenuation = { text = "Light \"Cube\"", minValue = 0.005, maxValue = 1.0, valueStep = 0.0025, },
 	fogDistance = { text = "Fog Distance", minValue = 100, maxValue = 1600, valueStep = 1.0, },
@@ -16,7 +16,7 @@ AmbientModPanelOptions = {
 	staticLOD = { text = "Static LOD" },
 	lightCulling = { text = "Light Culling" },
 	lightLimitFix = { text = "Light Limit Patch" },
-	overrideDayProgress = { text = "Day Progress Override", minValue = 0.005, maxValue = 1.0, valueStep = 0.005, },
+	overrideDayProgress = { text = "Time Progress", minValue = 0.005, maxValue = 1.0, valueStep = 0.005, },
 }
 
 function AmbientMod_OnLoad(self)
